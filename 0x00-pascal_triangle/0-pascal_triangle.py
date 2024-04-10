9156a1ef16c0ec6a0a1d122b61a3daf619506f55
Eman-Bakri <41126091+Eman-Bakri@users.noreply.github.com>
2024-04-11 02:41:48 +0300

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Pascal Triangle concept"""
+
+
+def pascal_triangle(n):
+    """Returns the n Pascal’s triangle:
+    """
+
+    if n <= 0:
+        return []
+    
+    """ empty array to hold the triangle data """
+    result_pascal = [[] for i in range(n)]
+
+    for element in range(n):
+        for column in range(element+1):
+            if(column < element):
+                if(column == 0):
+                    """ set first column elements to 1 """
+                    result_pascal[element].append(1)
+                else:
+                    result_pascal[element].append(pascal[element-1][column] + pascal[element-1][column-1])
+            elif(column == element):
+                """ set diagonal elements to 1 """
+                result_pascal[element].append(1)
+
+    return result_pascal
